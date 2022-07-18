2d94be62c02f9ebb734989a41f40ea4523859a91
Benjamin Bushman <benbushman98@gmail.com>
2022-07-17 23:28:54 -0600

    local storage work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,14 @@
 var APIKey = "9c2f191921ea4a448012e7d41b8872c0";
-var city = $("#city").val();
+var city = $("#city")
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-
-
+var searchBtn = $("#searchbtn").click(cityInput);
 
 function cityInput() {
-    alert(city)
-    console.log(queryURL);
-    console.log("test");
+    var ulEl = $('ul')
+    var liEl = $('<li>');
+    liEl.addClass('searchcities')
+    ulEl.append(liEl)
+    liEl.text(city.val())
+    window.localStorage.setItem("Searched City", city.val())
 }
-
-var searchBtn = document.getElementById("searchbtn")
-searchBtn.addEventListener("click", cityInput)
-
-
-// fetch(queryURL)
 
