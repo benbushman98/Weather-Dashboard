b71b95a6ce5fe69878e082c800a8a6ecb22171d8
Benjamin Bushman <benbushman98@gmail.com>
2022-07-17 21:35:25 -0600

    styling and html for search for a city

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,18 @@
 var APIKey = "9c2f191921ea4a448012e7d41b8872c0";
-var city = "Boston";
+var city = $("#city").val();
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
 
 
+function cityInput() {
+    alert(city)
+    console.log(queryURL);
+    console.log("test");
+}
 
-fetch(queryURL)
+var searchBtn = document.getElementById("searchbtn")
+searchBtn.addEventListener("click", cityInput)
 
-console.log (queryURL)
+
+// fetch(queryURL)
+
