84979d2367789a3737761e224a9af3b00b3e2019
Benjamin Bushman <benbushman98@gmail.com>
2022-07-17 23:49:40 -0600

    centered css lines

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,21 @@
 var APIKey = "9c2f191921ea4a448012e7d41b8872c0";
-var city = $("#city")
+var city = $("#city");
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 var searchBtn = $("#searchbtn").click(cityInput);
 
 function cityInput() {
-    var ulEl = $('ul')
+    var ulEl = $('ul');
     var liEl = $('<li>');
-    liEl.addClass('searchcities')
-    ulEl.append(liEl)
-    liEl.text(city.val())
-    window.localStorage.setItem("Searched City", city.val())
+
+    if(city.val() === "") {
+    alert("Enter City to Search Weather");
+    } 
+    else {
+    liEl.addClass('searchcities');
+    ulEl.append(liEl);
+    liEl.text(city.val());
+    window.localStorage.setItem("Searched City", city.val());
+    city.val("");
+    }
 }
 
