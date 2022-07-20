32aa28e2d2738ed773277bd0bf41a1b4b31f89f5
Benjamin Bushman <benbushman98@gmail.com>
2022-07-20 10:08:06 -0600

    worked on updating code for icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,66 +1,61 @@
 var APIKey = "9c2f191921ea4a448012e7d41b8872c0";
 var city = $("#city");
-var lat;
-var lon;
-var queryUrlCity = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial";
-var queryUrlLatLon = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly&appid=" + APIKey + "&units=imperial";
 var searchBtn = $("#searchbtn").click(getApiCity);
 var listCity = $('#listcity');
-
 var responseText = document.getElementById('response-text');
 
 
 function getApiCity() {
     city = $("#city").val()
-    var queryUrlCity = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial";
-    console.log(queryUrlCity)
+    queryUrlCity = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial";
+    // console.log(queryUrlCity)
     fetch(queryUrlCity)
     .then(function (response) {
-        console.log (response);
+        // console.log (response);
         return response.json();
     })
     .then(function (data) {
         var lat = (data.coord.lat);
         var lon = (data.coord.lon);
+        var iconWeather = data.weather[0].icon
         var imgEl = $('<img />',
             {id: "img",
-             src: "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png",
+             src: "https://openweathermap.org/img/wn/" + iconWeather + ".png",
              width: 200
             })
-        imgEl.appendTo($('#location'));
-        console.log(imgEl);
+            console.log(imgEl)
         //console.log(lat); console.log(data); console.log(lon);
         $('#location').text(data.name + " " + moment().format("MM/DD/YYYY"));
+        $('#location').append(imgEl);
         $('#temp').text('Temp: ' + data.main.temp + "°F");
         $('#wind').text('Wind: ' + data.wind.speed + " MPH");
         $('#humidity').text("Humidity: " + data.main.humidity + "%");
         getApiLatLon(lat, lon);
     },
-    
 )};
 
 function getApiLatLon(lat, lon) {
     queryUrlLatLon = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly&appid=" + APIKey + "&units=imperial";
     fetch(queryUrlLatLon)
     .then(function (response) {
-        console.log (response);
+        // console.log (response);
         return response.json();
     })
     .then(function (data) {
         var uvi = data.current.uvi
-        console.log(data)
+        // console.log(data)
         $('#currentday').show()
         $('#uvindex').text("UV Index: " + uvi)
         if (uvi <= 3) {
-            console.log("low")
+            // console.log("low")
             $('#uvindex').css("background-color", "green");
             $('#uvindex').css("color", "white");
         } else if (uvi >= 6) {
-            console.log("high")
+            // console.log("high")
             $('#uvindex').css("background-color", "red");
             $('#uvindex').css("color", "white");
         } else {
-            console.log("medium")
+            // console.log("medium")
             $('#uvindex').css("background-color", "yellow");
             $('#uvindex').css("color", "black");
         }
